dcdc9030c3e230a1bd67f2324b8ea365222030b6
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-12 16:06:59 +0530

    hello

---
+++ python collections/UserString.py
@@ -0,0 +1,12 @@
+from collections import UserString
+
+s = UserString("Hello World")
+print(s)
+print(s.data)
+
+
+
+from collections import UserString
+j = UserString("Jyotiraditya")
+print(j)
+print(j.data)
