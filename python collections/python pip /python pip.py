14bfaa2f5b2d79f988f55ceb07ce3f72e10f0a13
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-13 13:11:01 +0530

    init

---
+++ python collections/python pip /python pip.py
@@ -0,0 +1,4 @@
+#pip stands for pip install packages
+
+#pip stands for “Pip Installs Packages”.
+#It is Python’s official package manager, used to install, manage, and uninstall third-party Python packages from the Python Package Index (PyPI).
