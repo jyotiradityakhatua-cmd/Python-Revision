29e6c4ae29e5ed80840833b242e4ac41e069712a
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-13 16:06:32 +0530

    init

---
+++ venv/venv.py
@@ -0,0 +1,9 @@
+#venv stands for Virtual Environment.
+
+#It allows you to create an isolated Python environment with its own packages, separate from the global Python installation.
+
+#Useful to avoid package conflicts between projects.
+
+python -m venv venv
+
+venv/Scripts/activate.bat
