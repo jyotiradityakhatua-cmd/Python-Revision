3872b4894b2b2dee29fa8d3b1f3ea3c60c016412
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-13 10:39:45 +0530

    init

---
+++ iterator and generator/iterator.py
@@ -0,0 +1,21 @@
+i=0
+while i<=10:
+    print(i)
+    i+=1
+
+    #here i is iterator
+
+
+
+for i in range(1,10):
+    print(i)
+
+
+    for i in range(10):
+        print(i)
+        
+
+
+        list=["ram","shyam","mohan"]
+        for i in list:
+            print(i)
