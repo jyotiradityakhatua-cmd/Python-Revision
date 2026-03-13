aea23c9e1e555c402cd1b86661d9af271a5b3ae7
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-13 12:00:41 +0530

    init

---
+++ File Operations/file_append.py
@@ -0,0 +1,13 @@
+with open("kunal.txt", "a") as file:
+    l = file.write("\nThis is me")
+    print(l)
+
+with open("kunal.txt", "a") as file:
+    file.write("\nI am learning Python")
+
+with open("ayushman.txt","a") as file:
+    file.write("\nI am ayushman")
+
+with open("ayushman.txt","a")as file:
+    a=file.write("\nI am ayushman")
+    print(a)
