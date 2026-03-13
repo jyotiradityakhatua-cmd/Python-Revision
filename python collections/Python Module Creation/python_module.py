d4727a03c95b7b478eb08a87545bb8659f121c0d
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-13 12:57:42 +0530

    init

---
+++ python collections/Python Module Creation/python_module.py
@@ -0,0 +1,11 @@
+#A module in Python is a file containing Python code—functions, classes, variables, or runnable code—that can be imported and used in other Python programs.
+
+#File extension: .py
+
+#Purpose: Code reusability, organization, and maintainability
+
+#Example: Any file mymodule.py is a module.
+#Create a new file with .py extension, e.g., mymodule.py.
+
+#Define functions, variables, or classes in it.
+
