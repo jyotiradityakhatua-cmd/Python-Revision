474f48be2a267f64e90579ee86926fe7883c9e5e
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-13 11:09:15 +0530

    init

---
+++ Python Looping/while loop.py
@@ -0,0 +1,29 @@
+i=0
+while i<=10:
+    print("jyotir",i)
+    i+=1
+
+i=1
+while i<=100:
+    print(i)
+    i+=1
+
+count=1
+while count<=30:
+    print(count)
+    count+=2
+
+i=20
+while i>=1:
+    print("ayush",i)
+    i-=1
+
+i=1
+while i<=10:
+    print(i*3)
+    i+=1
+
+i=3
+while i<=30:
+    print(i)
+    i+=3
