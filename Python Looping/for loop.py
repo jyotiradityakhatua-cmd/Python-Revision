04cc6384424a4e466198784b446fed3483b516f2
jitendermobcoder-commits <jitender.mobcoder@mobcoder.com>
2026-03-13 11:29:15 +0530

    init

---
+++ Python Looping/for loop.py
@@ -0,0 +1,39 @@
+a=["mango","apple","strawberry"]
+for i in a:
+    print(i)
+
+b=[1,2,3,4,5,6,7,8,9]
+for i in b:
+    print(i)
+
+name ="Jyotiraditya Khatua"
+for i in name:
+    print(i) 
+
+name="Ayushman Tripathi"
+for i in name:
+    print(i)
+
+name="Kunal Gupta"
+for i in name:
+    print(i)
+
+for i in range(10):
+    print(i)
+
+for i in range(1,10):
+    print(i)
+ 
+for i in range(1,10,2):
+    print(i)
+
+numbers = [10, 20, 30]
+for i in range(len(numbers)):
+    print(numbers,i)
+
+
+a=input("enter the list:")
+i=0
+while i<(len(a)):
+    print(a[i])
+    i+=1
